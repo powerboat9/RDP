23921855b6243e327052f7d3ca8fa9bd5e3b3594
powerboat9 <powerboat9@users.noreply.github.com>
2015-12-09 21:14:13 -0500

    Create share.lua
    A library file for client and server.

---
+++ lib/share.lua
@@ -0,0 +1,9 @@
+function exchange(compName, side)
+    local modem = nil
+    if side then
+        modem = peripheral.wrap(side)
+    end
+    if not modem then
+        local modem = peripheral.find("modem")
+    end
+    modem.
